dd1d0bd76975cf7b117ce26fab5fb8baa65cb45d
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-01-04 16:14:23 +0300

    update api service

--- app/src/main/java/com/example/e_commerce_mobile/data/remote/Api Service.kt
+++ app/src/main/java/com/example/e_commerce_mobile/data/remote/Api Service.kt
@@ -1,16 +1,28 @@
 package com.example.e_commerce_mobile.data.remote
 
-import com.google.gson.annotations.SerializedName
+
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 
-interface ProductApiService {
-    @GET("product-management/product-hierarchy")
+interface ApiService {
+
+    // auth endpoints
+
+    @POST("user/register")
+    suspend fun registerUser(@Body userRegisterForm: UserRegisterForm): Response<UserRegisterResponseWithMessage>
+
+    @POST("user/login")
+    suspend fun loginUser(@Body userLoginForm: UserLoginForm): Response<UserLoginResponseWithMessage>
+
+
+    // product management endpoints
+    @GET("product-management/product-hierarchy/")
     suspend fun getProductHierarchy(): List<MainCategoryResponse>
 
-    @GET("product-management/{subCategoryId}/products-per-sub-category")
+    @GET("product-management/{subCategoryId}/products-per-sub-category/")
     suspend fun getProducts(@Path("subCategoryId")subCategoryId: Int): List<ProductResponse>
 
     @POST("product-management/{productId}/like/")
@@ -19,6 +31,10 @@ interface ProductApiService {
     @GET("product-management/{productId}/product-details/")
     suspend fun getProductDetails(@Path("productId") productId: Int): ProductResponse
 
+    @GET("product-management/banners/")
+    suspend fun getAdBanners(): List<BannerResponse>
 }
+
+
 
 

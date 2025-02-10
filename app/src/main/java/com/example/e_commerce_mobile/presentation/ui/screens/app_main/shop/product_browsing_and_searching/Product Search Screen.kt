fadf9354912d279de5e033042e3ea85a9fb5194a
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-02-10 10:17:56 +0300

    create product search screen

---
+++ app/src/main/java/com/example/e_commerce_mobile/presentation/ui/screens/app_main/shop/product_browsing_and_searching/Product Search Screen.kt
@@ -0,0 +1,10 @@
+package com.example.e_commerce_mobile.presentation.ui.screens.app_main.shop.product_browsing_and_searching
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProductSearchScreen(modifier: Modifier = Modifier) {
+
+    
+}

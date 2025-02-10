730b008d3ab3f2f3967237b58d472114dd629dc3
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-02-10 10:23:18 +0300

    update User account management viewmodel

--- app/src/main/java/com/example/e_commerce_mobile/presentation/viewmodel/UserAccountManagementViewModel.kt
+++ app/src/main/java/com/example/e_commerce_mobile/presentation/viewmodel/UserAccountManagementViewModel.kt
@@ -10,6 +10,9 @@ import com.example.e_commerce_mobile.data.remote.UserLoginForm
 import com.example.e_commerce_mobile.data.remote.UserLoginResponseWithMessage
 import com.example.e_commerce_mobile.data.remote.UserRegisterForm
 import com.example.e_commerce_mobile.data.repositories.AuthRepository
+import com.example.e_commerce_mobile.utils.CurrentUser
+import com.example.e_commerce_mobile.utils.UserPreferences
+import com.example.e_commerce_mobile.utils.UserSession
 import com.google.gson.annotations.SerializedName
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -26,25 +29,35 @@ import javax.inject.Inject
 
 @HiltViewModel
 class UserAccountManagementViewModel @Inject constructor(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val userSession: UserSession
 ): ViewModel() {
 
-    private val _loginStatus = MutableStateFlow(false)
-    val loginStatus: StateFlow<Boolean> get() = _loginStatus
+    val accessToken: StateFlow<String?> = userSession.accessToken
+    val isLoggedIn: StateFlow<Boolean> = userSession.isLoggedIn
+
+    init {
+
+        Log.d("ViewModel | accessToken", accessToken.value.toString())
+        Log.d("ViewModel | isLoggedIn", isLoggedIn.value.toString())
+    }
 
 
-
+    // Sign up fields
     var signUpFirstName by mutableStateOf("")
     var signUpLastName by mutableStateOf("")
     var signUpEmail by mutableStateOf("")
     var signUpPhoneNumber by mutableStateOf("")
-    var passwordResetEmail by mutableStateOf("")
     var signUpPassword by mutableStateOf("")
 
+    // Login fields
     var logInEmail by mutableStateOf("")
     var logInPassword by mutableStateOf("")
 
+    // Password reset fields
+    var passwordResetEmail by mutableStateOf("")
 
+    // UI binding functions
     val onSignUpFirstNameChange = { text: String -> signUpFirstName = text }
     val onSignUpLastNameChange = { text: String -> signUpLastName = text }
     val onSignUpEmailChange = { text: String -> signUpEmail = text }
@@ -54,19 +67,9 @@ class UserAccountManagementViewModel @Inject constructor(
     val onLogInEmailChange = { text: String -> logInEmail = text }
     val onLogInPasswordChange = { text: String -> logInPassword = text }
 
-
     val onPasswordResetEmailChange = { text: String -> passwordResetEmail = text }
 
-
-
-
-
-    // Function to update the base URL
-
-
-    // Example API interface
-
-
+    // Function to register a user
     fun registerUser() {
         val user = UserRegisterForm(
             firstName = signUpFirstName,
@@ -75,46 +78,75 @@ class UserAccountManagementViewModel @Inject constructor(
             phoneNumber = signUpPhoneNumber,
             password = signUpPassword
         )
-        // Launch a coroutine in the ViewModel scope
+        // Launch the network call in a coroutine
         viewModelScope.launch(Dispatchers.IO) {
-            Log.d("View Model | register function", "User: $user")
             try {
-                // design the system of
-                // Make the network call directly using the ApiService
                 val response = authRepository.registerUser(user)
 
-                // Check if the response is successful
-                Log.d("View Model | register function", "Response: $response ")
-
-                    // Handle the error response
-
+                if (response.isSuccessful) {
+                    // Handle successful registration logic
+                    Log.d("ViewModel | registerUser", "Registration successful: $response")
+                } else {
+                    // Handle unsuccessful registration (e.g., error messages)
+                    Log.d("ViewModel | registerUser", "Registration failed: ${response.errorBody()}")
+                }
             } catch (e: Exception) {
-                // Handle any exceptions, e.g., network errors
-                Log.d("View Model | register function", "Error during registration: ${e.localizedMessage}")
+                Log.d("ViewModel | registerUser", "Error during registration: ${e.localizedMessage}")
             }
         }
     }
 
-
+    // Function to log in a user
     fun loginUser() {
-        val user = UserLoginForm(
-            email = logInEmail,
-            password = logInPassword
-        )
-        // Launch a coroutine in the ViewModel scope
+        val user = UserLoginForm(email = logInEmail, password = logInPassword)
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                // Make the network call directly using the ApiService
                 val response = authRepository.loginUser(user)
+
                 if (response.isSuccessful) {
-                    Log.d("View Model | LogIn function", "Response: $response")
-                    _loginStatus.value = true
+                    response.body()?.accessToken?.let { token ->
+                        // Save token and update login state
+                        userSession.updateUserSession(response.body()?.accessToken.toString(),
 
+                            response.body()?.userId.toString()
+                        )
+
+                    }
+
+                    response.body()?.let { userData ->
+                        // Store the current user data for the session
+                        CurrentUser.token = userData.accessToken
+                        CurrentUser.userId = userData.userId.toString()
+                    }
+                } else {
+                    Log.d("ViewModel | loginUser", "Login failed: ${response.errorBody()}")
                 }
-
-                        // Show a success message or update UI accordingly
             } catch (e: Exception) {
-                Log.d("View Model | LogIn function", "Error during login: ${e.localizedMessage}")
+                Log.d("ViewModel | loginUser", "Error during login: ${e.localizedMessage}")
+            }
+        }
+    }
+
+    // Function to log out a user
+
+
+    fun logoutUser() {
+        // Make the API call to logout
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = authRepository.logoutUser(accessToken.value.toString())
+                if (response.isSuccessful) {
+                    // Call the method to clear the token from the local storage
+
+                    userSession.clearUserSession()
+                    CurrentUser.token = ""
+                    CurrentUser.userId = ""
+                } else {
+                    // Handle error if needed
+                    Log.d("Logout", "Logout failed: ${response.message()}")
+                }
+            } catch (e: Exception) {
+                Log.d("Logout", "Error during logout: ${e.localizedMessage}")
             }
         }
     }

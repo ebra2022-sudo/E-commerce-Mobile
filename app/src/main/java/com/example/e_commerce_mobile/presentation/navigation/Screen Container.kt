ba7d2bfdd0224333e1de50638e10c9f916871ff1
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-02-10 10:23:45 +0300

    update nav host

--- app/src/main/java/com/example/e_commerce_mobile/presentation/navigation/Screen Container.kt
+++ app/src/main/java/com/example/e_commerce_mobile/presentation/navigation/Screen Container.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -26,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -40,7 +42,9 @@ import com.example.e_commerce_mobile.R
 import com.example.e_commerce_mobile.presentation.ui.screens.app_main.bag.BagMainScreen
 import com.example.e_commerce_mobile.presentation.ui.screens.app_main.favorites.FavoritesMainScreen
 import com.example.e_commerce_mobile.presentation.ui.screens.app_main.home.HomeMainScreen
+import com.example.e_commerce_mobile.presentation.ui.screens.app_main.profile.MyOrdersScreen
 import com.example.e_commerce_mobile.presentation.ui.screens.app_main.profile.MyProfileScreen
+import com.example.e_commerce_mobile.presentation.ui.screens.app_main.profile.OrderDetailsScreen
 import com.example.e_commerce_mobile.presentation.ui.screens.app_main.shop.product_browsing_and_searching.CategoryAndSearchScreen
 import com.example.e_commerce_mobile.presentation.ui.screens.app_main.shop.product_display_and_information.Product3DPreviewScreen
 import com.example.e_commerce_mobile.presentation.ui.screens.app_main.shop.product_display_and_information.ProductDetailScreen
@@ -48,19 +52,25 @@ import com.example.e_commerce_mobile.presentation.ui.screens.app_main.shop.produ
 import com.example.e_commerce_mobile.presentation.ui.screens.auth.LogInScreen
 import com.example.e_commerce_mobile.presentation.ui.screens.auth.PasswordResetScreen
 import com.example.e_commerce_mobile.presentation.ui.screens.auth.SignUpScreen
+import com.example.e_commerce_mobile.presentation.viewmodel.ProductViewModel
+import com.example.e_commerce_mobile.presentation.viewmodel.UserAccountManagementViewModel
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun ScreenContainer(modifier: Modifier = Modifier, lifecycle: Lifecycle, context: Context) {
+    val viewModel = hiltViewModel<UserAccountManagementViewModel>()
+    val productViewModel = hiltViewModel<ProductViewModel>()
     val navController = rememberNavController()
-    val isAuthenticated = false // Replace with actual authentication logic
+    val isAuthenticated = viewModel.isLoggedIn.collectAsState().value
     val currentBackStackEntry = navController.currentBackStackEntryAsState()
     val currentRoute = currentBackStackEntry.value?.destination?.route
+
+
+// Accessing the previous back stack entry (previous screen)
+    val previousBackStackEntry = navController.previousBackStackEntry
+    val previousRoute = previousBackStackEntry?.destination?.route
+
     Log.d("CurrentRoute", currentRoute.toString())
     var selectedIndex by rememberSaveable { mutableIntStateOf(0) }
-
-
-    // Status bar height
 
     Scaffold(
         bottomBar = {
@@ -72,33 +82,34 @@ fun ScreenContainer(modifier: Modifier = Modifier, lifecycle: Lifecycle, context
         NavHost(
             modifier = modifier.padding(paddingValues),
             navController = navController,
-            startDestination = if (isAuthenticated) Screens.Home.route else Screens.Auth.route,
+            startDestination = if (isAuthenticated)  Screens.Home.route  else Screens.Auth.route,
             enterTransition = {slideInHorizontally { it }},
             exitTransition = {slideOutHorizontally{ -it}}
         ) {
             // Authentication flow
             navigation(startDestination = Screens.LogInScreen.route, route = Screens.Auth.route) {
-                composable(Screens.LogInScreen.route) { LogInScreen(navController = navController) }
-                composable(Screens.SignUpScreen.route) { SignUpScreen(navController = navController) }
-                composable(Screens.PasswordResetScreen.route) { PasswordResetScreen(navController = navController) }
+                composable(Screens.LogInScreen.route) { LogInScreen(navController = navController, viewModel = viewModel) }
+                composable(Screens.SignUpScreen.route) { SignUpScreen(navController = navController, viewModel = viewModel) }
+                composable(Screens.PasswordResetScreen.route) { PasswordResetScreen(navController = navController, viewModel = viewModel) }
             }
             // Main App flow
             navigation(startDestination = Screens.HomeMainScreen.route, route = Screens.Home.route) {
                 composable(
                     Screens.HomeMainScreen.route, enterTransition = {slideInHorizontally { it }},
-                    exitTransition = {slideOutHorizontally{ -it}}) { HomeMainScreen(navController = navController) }
+                    exitTransition = {slideOutHorizontally{ -it}}) { HomeMainScreen(navController = navController, viewModel = productViewModel) }
             }
             navigation(startDestination = Screens.ProductBrowsingAndSearching.route, route = Screens.Shop.route) {
                 navigation(startDestination = Screens.CategoryAndSearchScreen.route, route = Screens.ProductBrowsingAndSearching.route) {
                     composable(Screens.CategoryAndSearchScreen.route) {
                         CategoryAndSearchScreen(
-                            navController = navController
+                            navController = navController,
+                            viewModel = productViewModel
                         )
                     }
                 }
-                navigation(startDestination = Screens.SubCategoryProductsOverviewScreen.route, route = Screens.ProductDisplayAndInformation.route) {
+                navigation(startDestination = Screens.SubCategoryProductsOverviewScreen.route + "/{subCategoryId}", route = Screens.ProductDisplayAndInformation.route) {
                     composable(
-                        route = Screens.SubCategoryProductsOverviewScreen.withArgs("{subCategoryId}"),
+                        route = Screens.SubCategoryProductsOverviewScreen.route + "/{subCategoryId}",
                         arguments = listOf(
                             navArgument("subCategoryId") { type = NavType.StringType }
                         )
@@ -111,17 +122,18 @@ fun ScreenContainer(modifier: Modifier = Modifier, lifecycle: Lifecycle, context
                         // Call the screen composable
                         SubCategoryProductsOverviewScreen(
                             navController = navController,
-                            subCategoryId = subCategoryId.toInt()
+                            subCategoryId = subCategoryId.toInt(),
+                            viewModel = productViewModel
                         )
                     }
-                    composable(Screens.ProductDetailScreen.withArgs("{productId}"),
+                    composable(Screens.ProductDetailScreen.route + "/{productId}",
                         arguments = listOf(
                             navArgument("productId") { type = NavType.StringType }
                         )){
                         val productId = it.arguments?.getString("productId") ?: "3"
-                        ProductDetailScreen(navController = navController, productId = productId.toInt())
+                        ProductDetailScreen(navController = navController, productId = productId.toInt(), viewModel = productViewModel)
                     }
-                    composable(Screens.Product3DModelScreen.withArgs("{modelUrl}"),
+                    composable(Screens.Product3DModelScreen.route + "/{modelUrl}",
                         arguments = listOf(
                             navArgument("modelUrl") { type = NavType.StringType }
                         )) {
@@ -131,20 +143,27 @@ fun ScreenContainer(modifier: Modifier = Modifier, lifecycle: Lifecycle, context
             }
             // sample of the  ste of he  current valeu of the  sate of the design state of then
             navigation(startDestination = Screens.BagMainScreen.route, route = Screens.Bag.route) {
-                composable(Screens.BagMainScreen.route) { BagMainScreen() }
+                composable(Screens.BagMainScreen.route) { BagMainScreen(viewModel = productViewModel) }
             }
             navigation(startDestination = Screens.FavoritesMainScreen.route, route = Screens.Favorites.route) {
-                composable(Screens.FavoritesMainScreen.route) { FavoritesMainScreen() }
+                composable(Screens.FavoritesMainScreen.route) { FavoritesMainScreen(navController = navController, viewModel = productViewModel) }
             }
             navigation(startDestination = Screens.ProfileMainScreen.route, route = Screens.Profile.route) {
-                composable(Screens.ProfileMainScreen.route) { MyProfileScreen() }
+                composable(Screens.ProfileMainScreen.route) { MyProfileScreen(navController = navController) }
+                composable(Screens.MyOrdersScreen.route) { MyOrdersScreen(navController = navController) }
+                composable(Screens.OrderDetailsScreen.route + "/{orderId}",
+                    arguments = listOf(navArgument("orderId") { type = NavType.StringType })) {
+                    val orderId = it.arguments?.getString("orderId") ?: ""
+                    Log.d("OrderDetailsScreen", orderId)
+                    OrderDetailsScreen(navController = navController,viewModel = productViewModel, orderId = orderId.toInt())
+                }
             }
         }
     }
 }
 
 @Composable
-fun BottomNavigationBar(modifier: Modifier = Modifier, navController: NavController, selectedIndex: Int, onItemSelected: (Int) -> Unit = {}) {
+fun BottomNavigationBar(navController: NavController, selectedIndex: Int, onItemSelected: (Int) -> Unit = {}) {
 
     val items = listOf(Screens.Home.route, Screens.Shop.route, Screens.Bag.route, Screens.Favorites.route, Screens.Profile.route)
     val unSelectedIconResources = listOf(R.drawable.outlined_home, R.drawable.outlined_shop, R.drawable.outlined_bag, R.drawable.outlined_favorite, R.drawable.outlined_profile)
@@ -210,7 +229,7 @@ fun shouldShowBottomNavigation(currentRoute: String?): Boolean {
     return currentRoute in listOf(
         Screens.HomeMainScreen.route,
         Screens.CategoryAndSearchScreen.route,
-        Screens.SubCategoryProductsOverviewScreen.withArgs("{subCategoryId}"),
+        Screens.SubCategoryProductsOverviewScreen.route,
         Screens.BagMainScreen.route,
         Screens.FavoritesMainScreen.route,
         Screens.ProfileMainScreen.route
@@ -224,7 +243,6 @@ fun shouldShowBottomNavigation(currentRoute: String?): Boolean {
 @Preview
 @Composable
 private fun Preview() {
-    //ScreenContainer()
-    
+
 }
 
